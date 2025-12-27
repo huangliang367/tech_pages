62bd1373be480fd6a373d4b6e081411b31606154
Huang Liang <584782297@qq.com>
2025-12-27 22:38:45 +0800

    update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/tech_pages/',
   title: "Technology documents",
   description: "Technology documents",
   themeConfig: {
